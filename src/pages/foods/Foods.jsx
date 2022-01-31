40f4528f900d08b8ad08b66be7a45130da1bdce5
Lucas Alves da Silva <lucasdolohov@gmail.com>
2022-01-31 15:08:06 -0300

    [Grupo 27] tela de login (#181)
    * feat: tela de login
    
    * feat: paleta de cor css

---
+++ src/pages/foods/Foods.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Foods() {
+  return (
+    <p>comidas</p>
+  );
+}
+
+export default Foods;
---
+++ src/styles.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+export const MainLogin = styled.div`
+  height: 640px;
+  padding: 110px 0;
+  background-color: #F5DEB3;
+  `;
+
+export const Welcome = styled.p`
+  background-color: #F4A460;
+  width: 200px;
+  margin: 0px auto 0px auto;
+  color: #000000;
+  text-align: center;
+  font-size: 30px;
+  border-radius: 18px 18px 0 0;
+`;
+
+export const CodeAndCooking = styled.h1`
+  margin: 7px;
+  margin-top: 0px;
+  color: #000000;
+  text-align: center;
+  background-color: #F4A460;
+  border-radius: 18px;
+  padding: 5px;
+  padding-top: 0px;
+`;
+
+export const Form = styled.form`
+  margin: 40% auto;
+  max-Width: 80%;
+  height: 150px;
+  input {
+    display: block;
+    margin: 10px auto;
+    max-width: 100%;
+    height: 35px;
+    border-radius: 8px;
+    border: 1px solid #333;
+  }
+  button {
+    display: block;
+    margin: 1px auto;
+    width: 74%;
+    border-radius: 8px;
+  }
+`;
